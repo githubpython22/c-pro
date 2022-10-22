9c516b72d0d0202580057ce4b31ab5bb916f1230
githubpython22 <116423528+githubpython22@users.noreply.github.com>
2022-10-22 12:10:41 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n, k;
+    cin>>n>>k;
+    int a[n];
+    for(int i=0; i<n; i++){
+        cin>>a[i];
+    }
+    
+    for(int i=0; i<n; i++){
+        int res=1;
+        int curr = a[i];
+        for(int j=i+1; j<n; j++){
+            if(curr%1000<a[j]%1000 && 0<=a[j]-curr && a[j]-curr<k*1000){
+                res++;
+                curr=a[j];
+            }
+        }
+        ans = max(ans, res);
+    }
+    cout<<ans;
+    return 0;
+}
